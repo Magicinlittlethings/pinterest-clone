383ac2ae5669956be1c6220bc11b56d08e0c2990
lawal <ladeholar@gmail.com>
2022-07-02 07:03:36 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#E60023',
+        'dark': '#111111',
+        'light':'EFEFEF',
+        'lightHover': '#d7d7d7',
+        'muted': '#717171'
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
